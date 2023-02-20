729bddcc083de30538b906c8e95574d98dbe6329
buchonsi <buchonsi@naver.com>
2023-02-20 17:56:19 +0900

    Revert "update semaphore test code"
    This reverts commit 4e74bd89863bdf6bc43e484800a8c858b8765b74.

--- src/main/java/SemaphoreQueue.java
+++ src/main/java/SemaphoreQueue.java
@@ -1,7 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class SemaphoreQueue<T> {
 
@@ -27,15 +26,9 @@ public class SemaphoreQueue<T> {
 
     //poll
     public T poll(){
-        T result;
-        try {
-            semaphore.acquire();
-            result = queue.poll();
-            semaphore.release();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        synchronized (queue){
+            return queue.poll();
         }
-        return result;
     }
 
     public T peek(){
--- test/SemaphoreQueueTest.java
+++ test/SemaphoreQueueTest.java
@@ -1,22 +1,17 @@
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Semaphore;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SemaphoreQueueTest {
 
     @Test
-    @DisplayName("Semaphore를 이용한 offer")
-    void offerTest(){
-        //given
-        Semaphore semaphore = new Semaphore(1);
+    void semaphoreTest(){
+        Semaphore semaphore = new Semaphore(2);
+
         SemaphoreQueue semaphoreQueue = new SemaphoreQueue(semaphore);
 
-        //when
         Thread t1 = new Thread(()->{
             for(int i=0; i<100; i++){
                 semaphoreQueue.offer("test" + i);
@@ -35,49 +30,13 @@ class SemaphoreQueueTest {
         try {
             t1.join();
             t2.join();
+            System.out.println(semaphoreQueue.toString());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        assertThat(semaphoreQueue.size()).isEqualTo(200);
+
+
     }
-
-    @Test
-    @DisplayName("Semaphore를 이용한 poll")
-    void pollTest(){
-        //given
-        Semaphore semaphore = new Semaphore(1);
-        SemaphoreQueue semaphoreQueue = new SemaphoreQueue(semaphore);
-
-        for(int i=0; i<200; i++){
-            semaphoreQueue.offer("test" + i);
-        }
-
-        //when
-        Thread t1 = new Thread(()->{
-            for(int i=0; i<100; i++){
-                semaphoreQueue.poll();
-            }
-        });
-
-        Thread t2 = new Thread(()->{
-            for(int i=100; i<200; i++){
-                semaphoreQueue.poll();
-            }
-        });
-
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        assertThat(semaphoreQueue.size()).isEqualTo(0);
-    }
-
 
 }
